79182fb70b81b297be9be319d857ebe95142e3a9
hiiamuu <amank233@gmail.com>
2023-07-08 16:20:32 +0530

    Working on User Section 2

--- src/components/pages/user/User.js
+++ src/components/pages/user/User.js
@@ -8,6 +8,12 @@ export default function User() {
                 <h1 className="userTitle">Edit User</h1>
                 <button className="userAddButton">Create</button>
             </div>
+            <div className="userContainer">
+                <div className="userShow">
+                </div>
+                <div className="userUpdate">
+                </div>
+            </div>
         </div>
     )
 }
